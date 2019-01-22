e6087144385e81417dc58fc4949c0535a4422b9b
nghiand <nghiand@gmail.com>
2019-01-22 16:32:49 +0700

    a

---
+++ FactoryMethodPattern/src/BankType.java
@@ -0,0 +1,3 @@
+public enum BankType {
+    TPBANK, VIETCOMBANK
+}
---
+++ FactoryMethodPattern/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Bank bank = BankFactory.getBank(BankType.TPBANK);
+        System.out.print(bank.getBankName());
+    }
+}
